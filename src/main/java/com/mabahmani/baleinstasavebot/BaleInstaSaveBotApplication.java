238f8a917bbeaf70beebd2f038ef07fd4b2ebd15
mabahmani <aminbahmani10@gmail.com>
2022-06-23 16:02:29 +0430

    init

---
+++ src/main/java/com/mabahmani/baleinstasavebot/BaleInstaSaveBotApplication.java
@@ -0,0 +1,13 @@
+package com.mabahmani.baleinstasavebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaleInstaSaveBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaleInstaSaveBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mabahmani/baleinstasavebot/BaleInstaSaveBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabahmani.baleinstasavebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaleInstaSaveBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
