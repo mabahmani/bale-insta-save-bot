999180cd1610e36d3baec9afe43ce247cf50159c
mabahmani <aminbahmani10@gmail.com>
2022-06-23 16:28:48 +0430

    test controller to show token

--- src/main/java/com/mabahmani/baleinstasavebot/BaleInstaSaveBotApplication.java
+++ src/main/java/com/mabahmani/baleinstasavebot/BaleInstaSaveBotApplication.java
@@ -1,13 +1,26 @@
 package com.mabahmani.baleinstasavebot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class BaleInstaSaveBotApplication {
+public class BaleInstaSaveBotApplication implements CommandLineRunner {
+
+    private final Environment env;
+    public static String botToken;
+
+    public BaleInstaSaveBotApplication(Environment env) {
+        this.env = env;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BaleInstaSaveBotApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        botToken = env.getProperty("token");
+    }
 }
--- src/main/java/com/mabahmani/baleinstasavebot/controller/MainController.java
+++ src/main/java/com/mabahmani/baleinstasavebot/controller/MainController.java
@@ -1,2 +1,14 @@
-package com.mabahmani.baleinstasavebot.controller;public class MainController {
+package com.mabahmani.baleinstasavebot.controller;
+
+import com.mabahmani.baleinstasavebot.BaleInstaSaveBotApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String getToken(){
+        return BaleInstaSaveBotApplication.botToken;
+    }
 }
